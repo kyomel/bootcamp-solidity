4fe589be4bc880ca94d79718628c534e2148d3f3
kyomel <michaellapandio04@gmail.com>
2025-05-04 19:50:46 +0700

    add src to repository

---
+++ src/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("KyoCoin", "KYC") {
+
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
